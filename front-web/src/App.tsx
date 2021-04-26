c9623d9f7aad99161cfb8da1e1e3c536aa5e865c
Rafael Marinheiro <rsmarinheiro007@gmail.com>
2021-04-25 22:58:21 -0300

    Adicionando componente Header

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './componentes/Header';
+
 
 function App() {
   return (
-    <h1>Hello World!!!</h1>
+    <div>
+        <Header></Header>
+    </div>
+
+
   );
 }
 
