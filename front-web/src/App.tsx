8762137164affeee42d8ec67e15dfd7bfde966ab
Rafael Marinheiro <rsmarinheiro007@gmail.com>
2021-04-26 17:45:25 -0300

    Criando navegação com Rotas

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -3,14 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './componentes/Header';
 import Home from './pages/home';
+import Routes from './routes';
 
 
 function App() {
   return (
-    <div>
-        <Header></Header>
-        <Home></Home>
-    </div>
+   <Routes/>
 
 
   );
--- front-web/src/pages/home/index.tsx
+++ front-web/src/pages/home/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import {ReactComponent as Arrow} from '../../Assets/arrow.svg';
-import {ReactComponent as Gamer} from '../../Assets/GAMER.svg';
+import { Link } from 'react-router-dom';
+import { ReactComponent as Arrow } from '../../Assets/arrow.svg';
+import { ReactComponent as Gamer } from '../../Assets/GAMER.svg';
 import './styles.css';
 
-const home  = () =>(
+const home = () => (
     <div className="home-container">
         <div className="home-text">
             <h1 className="home-text-tilte">
@@ -12,14 +13,16 @@ const home  = () =>(
             <h3 className="home-text-subtitle">
                 Clique no botão abaixo e saiba quais são os jogos que os gamers estão escolhendo!
             </h3>
-            <div className="home-actions">
-                <button className="home-btn">Quero Saber Quais São?</button>
-                <div className="home-btn-icon">
-                <Arrow/>
-              </div>
-            </div>
+            <Link to="/records">
+                <div className="home-actions">
+                    <button className="home-btn">Quero Saber Quais São?</button>
+                    <div className="home-btn-icon">
+                        <Arrow />
+                    </div>
+                </div>
+            </Link>
         </div>
-        <Gamer className="home-image"/>
+        <Gamer className="home-image" />
     </div>
 )
 
